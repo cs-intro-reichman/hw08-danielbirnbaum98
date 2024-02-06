ac871c8fbd0a6be7fc9f7a269ddb339aa0b728b4
Daniel <daniel.birnbaum@post.runi.ac.il>
2024-02-06 18:06:11 +0200

    Second2.0

--- PlayList.java
+++ PlayList.java
@@ -111,6 +111,7 @@ class PlayList {
      *  does nothing and returns -1. */
     public void remove(int i) {
         if (i > 0 && i < maxSize && size != 0){
+            tracks[i] = null;
             for (int j = i; j < size - 2; j++) {
                 tracks[j] = tracks[j + 1];
             }
